9719b4728c691c2fed7e930ee1618d423cd8cc40
VitmiTor <torremvictor@gmail.com>
2023-07-25 12:28:29 -0500

    ejercicio5

---
+++ ejercicios/ejercicio3.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const {
+  generarEnteroAleatorio,
+  generarDecimalAleatorio,
+} = require("../utilities/randomUtilities");
+
+const promedioPonderado = (cantidadN, arrayNumbers, arrayPesos) => {
+  let sumatoria = 0;
+  for (let i = 0; i < cantidadN; i++) {
+    const numeroAleatorio = generarEnteroAleatorio(100, 1000);
+    const pesosAleatorios = generarEnteroAleatorio(1, 50);
+    arrayNumbers.push(numeroAleatorio);
+    arrayPesos.push(pesosAleatorios);
+    sumatoria += arrayNumbers[i] * arrayPesos[i];
+  }
+  return sumatoria / cantidadN;
+};
+
+const imprimirMensaje = (arrayNumbers, arrayPesos, promedio) => {
+  console.log(
+    `el array de numeros es ${arrayNumbers} y el array de Pesos ${arrayPesos} y su promedio ponderado es ${promedio}`
+  );
+};
+
+const arrayNumbers = [];
+const arrayPesos = [];
+const cantidadN = generarEnteroAleatorio(1, 10);
+const resultado = promedioPonderado(cantidadN, arrayNumbers, arrayPesos);
+imprimirMensaje(arrayNumbers, arrayPesos, resultado);
---
+++ ejercicios/ejercicio5.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const {
+  generarEnteroAleatorio,
+  generarDecimalAleatorio,
+} = require("../utilities/randomUtilities");
+
+const calcularMediaGeometrica = (arrayNumbers, cantidadN) => {
+  let producto = 1;
+  for (let i; i < cantidadN; i++) {
+    const numeroAleatorio = generarEnteroAleatorio(100, 999);
+    arrayNumbers.push(numeroAleatorio);
+    producto = producto * arrayNumbers[i];
+  }
+  return Math.pow(producto, 1 / cantidadN);
+};
+
+const imprimirMensaje = (mediageometrica) => {
+  console.log(`la media geometrica es ${mediageometrica}`);
+};
+
+const numbers = [];
+const cantidadN = generarEnteroAleatorio(1, 10);
+const mediaGeometrica = calcularMediaGeometrica(numbers, cantidadN);
+imprimirMensaje(mediaGeometrica);
